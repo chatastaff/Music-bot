fcb39b82d740e7b8a510fa05f781d14b75c1e09e
MrGullani <80585623+MrGullani@users.noreply.github.com>
2021-07-22 16:27:25 +0300

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -9,9 +9,14 @@ module.exports = {
 
     discord: {
         token: 'TOKEN',
+        token: 'ODY3NzIwMjcxMzg0ODA1NDE2.YPlNeg.-4Q65K0aOuJAaddWxz4rw33iFD4',
         prefix: 'PREFIX',
+        prefix: '*',
+        activity: 'ACTIVITY',
         activity: 'ACTIVITY',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+-};
+},
 };
